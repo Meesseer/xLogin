3be09ad7783824ea2c5f1749889d242bb6d25de4
Ammar Khan <ammarasu@gmail.com>
2024-11-03 16:59:24 +0530

    m

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import { useState } from 'react';
 
 function App() {
 
-  const username = "Ammar";
-  const password = "NewPassword"
+  const username = "user";
+  const password = "password"
   const [form, setForm] = useState({
     username: "",
     password: "",
   })
-  const [details, setDetails] = useState(false)
   const [isSubmitted, setIssubmitted] = useState(false)
   const [error, setError] = useState("");
 
@@ -22,10 +21,8 @@ function App() {
   const handleSubmit =(e) =>{
     e.preventDefault()
     if(form.username === username && form.password === password){
-      setDetails(true)
       setIssubmitted(true)
     }else{
-      setDetails(false)
       setError("Invalid username or password. Please try again.");
     }
   }
@@ -35,13 +32,13 @@ function App() {
       {isSubmitted ? (
         <div>
           <h1>Login Page</h1>
-          <h3>Welcome {form.username}</h3>
+          <h3>Welcome, user</h3>
         </div>
       ):( <form onSubmit={handleSubmit}>
         <div>
-          Username: <input type='text' label="username" name='username' value={form.username} onChange={handleChange} required></input>
+        <label htmlFor="username">Username:</label> <input type='text' label="username" name='username' value={form.username} onChange={handleChange} required></input>
         </div>
-        <div>Password: <input type='password' label="password" name='password' value={form.password} onChange={handleChange} required></input></div>
+        <div><label htmlFor="password">Password:</label><input type='password' label="password" name='password' value={form.password} onChange={handleChange} required></input></div>
         <div><button type='submit'>Submit</button></div>
         {error && <div style={{ color: 'red' }}>{error}</div>}
       </form>)} 
