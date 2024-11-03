baf685b8f12c897f2ffb437662d139d35bbd25e3
Ammar Khan <ammarasu@gmail.com>
2024-11-03 15:22:03 +0530

    xlogin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const username = "Ammar";
+  const password = "NewPassword"
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+  })
+  const [details, setDetails] = useState(false)
+  const [isSubmitted, setIssubmitted] = useState(false)
+
+  const handleChange = (e)=>{
+    const {name, value} = e.target
+    setForm(prev => ({
+      ...prev, [name]: value
+    }))
+  }
+  const handleSubmit =(e) =>{
+    e.preventDefault()
+    if(form.username === username && form.password === password){
+      setDetails(true)
+      setIssubmitted(true)
+    }else{
+      setDetails(false)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isSubmitted ? (
+        <div>
+          <h1>Login Page</h1>
+          <h3>Welcome {form.username}</h3>
+        </div>
+      ):( <form onSubmit={handleSubmit}>
+        <div>
+          Username: <input type='text' name='username' value={form.username} onChange={handleChange} required></input>
+        </div>
+        <div>Password: <input type='password' name='password' value={form.password} onChange={handleChange} required></input></div>
+        <div><button type='submit'>Submit</button></div>
+      </form>)} 
     </div>
   );
 }
